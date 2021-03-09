7f8a4aa0b2500caec834c8b5f8b40bac9bb4d5b9
Jason-Quiroga <60979762+Jason-Quiroga@users.noreply.github.com>
2021-03-08 21:53:41 -0500

    Update secure-ssh.sh
    Updating the file to include the SSH code

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -3,5 +3,10 @@
 #Creates a new SSH user using $1 parameter
 #Adds a public key from the local repo or curled from the remote repo
 #Removes Roots ability to SSH in
-echo "ALL YOUR CODE GOES HERE"
 
+sudo useradd -m -d /home/$1 -s /bin/bash $1
+sudo mkdir /home/$1/.ssh
+sudo cp SYS265/linux/public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys
+sudo chmod 700 home/$1/.ssh
+sudo chmod 600 home/$1/.ssh/authorized_keys
+sudo chown -R $1:$1 /home/$1/.ssh
