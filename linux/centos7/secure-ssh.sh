d211270fc9e6971ee6dfd8b1109f8c58ba925596
Jason-Quiroga <60979762+Jason-Quiroga@users.noreply.github.com>
2021-03-08 21:59:11 -0500

    Update secure-ssh.sh

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -7,6 +7,6 @@
 sudo useradd -m -d /home/$1 -s /bin/bash $1
 sudo mkdir /home/$1/.ssh
 sudo cp /home/jason/SYS265/linux/public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys
-sudo chmod 700 home/$1/.ssh
-sudo chmod 600 home/$1/.ssh/authorized_keys
+sudo chmod 700 /home/$1/.ssh
+sudo chmod 600 /home/$1/.ssh/authorized_keys
 sudo chown -R $1:$1 /home/$1/.ssh
