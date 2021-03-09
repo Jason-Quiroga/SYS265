a036084f8bf2482a6451a17956dee8bd6e1cb0ce
Jason-Quiroga <jason.quiroga@mymail.champlain.edu>
2021-03-08 21:21:16 -0500

    Added directories for sorting and secure-ssh.sh

---
+++ linux/centos7/secure-ssh.sh
@@ -0,0 +1,7 @@
+#Secure-SSH.sh
+#Author: Jason Quiroga
+#Creates a new SSH user using $1 parameter
+#Adds a public key from the local repo or curled from the remote repo
+#Removes Roots ability to SSH in
+echo "ALL YOUR CODE GOES HERE"
+
